a38f2548c93e80a7fb065110fc2bb1a5011e9359
PL <petro.lototskyi@gmail.com>
2025-02-25 12:02:33 -0500

    Initial commit

---
+++ lecture_4/test.py
@@ -0,0 +1,15 @@
+# import os
+# os.system("Xvfb :99 -screen 0 1024x768x16 &")
+# os.environ["DISPLAY"] = ":99"
+
+import lecture_4.graphics as graphics
+# import tkinter as tk
+# from pyvirtualdisplay import Display
+import tkinter as tk
+
+win = graphics.GraphWin("My Window", 500, 500) 
+# Wait for user interaction before closing (prevents instant close)
+win.getMouse()  # Click to close
+win.close() 
+
+
