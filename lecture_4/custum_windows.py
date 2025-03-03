ad804424cd43a46f122c1ebb9e085f23c6d2b095
PL <petro.lototskyi@gmail.com>
2025-03-03 16:27:18 -0500

    Custum window graphic program.

---
+++ lecture_4/custum_windows.py
@@ -0,0 +1,22 @@
+from graphics import * 
+# import graphics as graphics
+from graphics import Circle, GraphWin, Point, Text, Rectangle, Line
+
+
+# create a default 200x200 window 
+win=GraphWin("Tic-Tac-Toe") 
+
+# set coordinates to go from (0,0) in the lower left 
+#to (3,3) in the upper right . 
+win.setCoords(0.0, 0.0, 3.0, 3.0) 
+
+# Draw vertical lines 
+Line(Point(1 ,0) , Point(1 ,3)) .draw(win) 
+Line(Point(2,0) , Point(2,3)) .draw(win) 
+
+# Draw horizontal lines 
+Line(Point(0,1) , Point(3,1)) .draw(win) 
+Line(Point(0,2) , Point(3,2)) .draw(win) 
+
+input("Press <Enter> to quit") 
+win. close() 
